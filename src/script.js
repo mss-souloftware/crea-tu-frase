5765e0b27213d4b645d60b132175989604b0d39a
mss-souloftware <sufyan.souloftware@gmail.com>
2024-06-25 17:51:12 +0500

    star added

--- src/script.js
+++ src/script.js
@@ -4,27 +4,27 @@
     $(document).ready(function () {
         const keyMap = {
             48: '0.png', 49: '1.png', 50: '2.png', 51: '3.png', 52: '4.png', 53: '5.png',
-            54: '6.png', 55: '7.png', 56: '8.png', 57: '9.png', 32: 'heart.png', 65: 'a.png',
-            66: 'b.png', 67: 'c.png', 68: 'd.png', 69: 'e.png', 70: 'f.png', 71: 'g.png',
-            72: 'h.png', 73: 'i.png', 74: 'j.png', 75: 'k.png', 76: 'l.png', 77: 'm.png',
-            78: 'n.png', 79: 'o.png', 80: 'p.png', 81: 'q.png', 82: 'r.png', 83: 's.png',
-            84: 't.png', 85: 'u.png', 86: 'v.png', 87: 'w.png', 88: 'x.png', 89: 'y.png',
-            90: 'z.png'
+            54: '6.png', 55: '7.png', 56: '8.png', 57: '9.png', 32: { heart: 'heart.png', star: 'star.png' },
+            65: 'a.png', 66: 'b.png', 67: 'c.png', 68: 'd.png', 69: 'e.png', 70: 'f.png',
+            71: 'g.png', 72: 'h.png', 73: 'i.png', 74: 'j.png', 75: 'k.png', 76: 'l.png',
+            77: 'm.png', 78: 'n.png', 79: 'o.png', 80: 'p.png', 81: 'q.png', 82: 'r.png',
+            83: 's.png', 84: 't.png', 85: 'u.png', 86: 'v.png', 87: 'w.png', 88: 'x.png',
+            89: 'y.png', 90: 'z.png'
         };
 
-        function generateImages(text, $typewriterInner) {
+        function generateImages(text, $typewriterInner, spaceSymbol) {
             $typewriterInner.empty();
-            const words = text.split('♥');
+            const words = text.split(spaceSymbol);
             words.forEach((word, index) => {
                 const $wordDiv = $('<div>').addClass('word');
                 for (const char of word) {
                     let keyCode;
-                    if (char === '♥') {
+                    if (char === spaceSymbol) {
                         keyCode = 32;
                     } else {
                         keyCode = char.toUpperCase().charCodeAt(0);
                     }
-                    const imgFileName = keyMap[keyCode];
+                    const imgFileName = typeof keyMap[keyCode] === 'object' ? keyMap[keyCode][$('#letras').val()] : keyMap[keyCode];
                     if (imgFileName) {
                         const imgPath = `http://localhost/wordpress/wp-content/plugins/crea-tu-frase/img/letters/${imgFileName}`;
                         const $img = $('<img>').attr('src', imgPath).addClass('letter-img');
@@ -38,7 +38,7 @@
                 }
                 $typewriterInner.append($wordDiv);
                 if (index < words.length - 1) {
-                    const imgPath = `http://localhost/wordpress/wp-content/plugins/crea-tu-frase/img/letters/${keyMap[32]}`;
+                    const imgPath = `http://localhost/wordpress/wp-content/plugins/crea-tu-frase/img/letters/${keyMap[32][$('#letras').val()]}`;
                     const $img = $('<img>').attr('src', imgPath).addClass('letter-img');
                     $typewriterInner.append($img);
                 }
@@ -47,10 +47,11 @@
 
         function attachInputHandler($input, $typewriterInner) {
             $input.on('input', function () {
+                const selectedSymbol = $('#letras').val() === 'heart' ? '♥' : '✯';
                 let inputText = $(this).val();
-                inputText = inputText.replace(/ /g, '♥').toUpperCase();
+                inputText = inputText.replace(/ /g, selectedSymbol).toUpperCase();
                 $(this).val(inputText);
-                generateImages(inputText, $typewriterInner);
+                generateImages(inputText, $typewriterInner, selectedSymbol);
             });
         }
 
