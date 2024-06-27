7ebf4dde149384e4dd2652894f998e3837bac24e
mss-souloftware <sufyan.souloftware@gmail.com>
2024-06-27 15:33:18 +0500

    cancelation order

--- src/script.js
+++ src/script.js
@@ -71,16 +71,29 @@
 
         $("#addNewFrase").click(function () {
             const newTypewriterInnerId = `typewriterInner_${typewriterCounter++}`;
+            const newFrasePanelId = `frasePanel_${typewriterCounter}`;
+
             const $newFrasePanel = $(`
-                <div class="frasePanel">
-                    <input class="fraseInput" type="text" placeholder="Escriba su frase aquí.." required="">
-                </div>
-            `);
+        <div class="frasePanel" id="${newFrasePanelId}">
+            <div class="closeBtnTyper">
+                <svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12ZM8.96963 8.96965C9.26252 8.67676 9.73739 8.67676 10.0303 8.96965L12 10.9393L13.9696 8.96967C14.2625 8.67678 14.7374 8.67678 15.0303 8.96967C15.3232 9.26256 15.3232 9.73744 15.0303 10.0303L13.0606 12L15.0303 13.9696C15.3232 14.2625 15.3232 14.7374 15.0303 15.0303C14.7374 15.3232 14.2625 15.3232 13.9696 15.0303L12 13.0607L10.0303 15.0303C9.73742 15.3232 9.26254 15.3232 8.96965 15.0303C8.67676 14.7374 8.67676 14.2625 8.96965 13.9697L10.9393 12L8.96963 10.0303C8.67673 9.73742 8.67673 9.26254 8.96963 8.96965Z" fill="#E64C3C" />
+                </svg>
+            </div>
+            <input class="fraseInput" type="text" placeholder="Escriba su frase aquí.." required="">
+        </div>
+    `);
             $('.fraseWrapper').append($newFrasePanel);
             const $newTypewriterInner = $(`<div class="typewriterInner" id="${newTypewriterInnerId}"></div>`);
             $('#typewriter').append($newTypewriterInner);
             const $newInput = $newFrasePanel.find('.fraseInput');
             attachInputHandler($newInput, $newTypewriterInner);
+
+            // Attach click event handler to the close button
+            $newFrasePanel.find('.closeBtnTyper').click(function () {
+                $newFrasePanel.remove(); // Remove the frase panel
+                $newTypewriterInner.remove(); // Remove the typewriter inner element
+            });
         });
 
         let screenshotPaths = [];
@@ -239,7 +252,10 @@
 
         setProgressBar(current);
 
-        $("#picDate").flatpickr();
+        $("#picDate").flatpickr({
+            minDate: "today"
+        });
+
 
         $(".next").click(function () {
             current_fs = $(this).parents('fieldset');
